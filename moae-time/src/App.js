452bfc850dbe1b0e8123fe939e3921643725913e
leephoter <estarg51@gmail.com>
2022-01-21 15:47:59 +0900

    create directory tree

---
+++ moae-time/src/App.js
@@ -0,0 +1,13 @@
+import { HomePage, BoardPage } from "./pages";
+
+function App() {
+  // page Components만 관리
+  return (
+    <>
+      <HomePage />
+      <BoardPage />
+    </>
+  );
+}
+
+export default App;
