0d87dc78d594411a96d879cd20056800a1a479cd
김성제 <rlatjdwp0211@naver.com>
2022-01-24 14:08:18 +0900

    Modify(sungje/Layout-header):create-layout-header #16

--- moae-time/src/App.js
+++ moae-time/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { HomePage, BoardPage } from "./pages";
 import { Routes, Route } from "react-router-dom";
-import DummyLayout from "./style/layout/DummyLayout";
+import Layout from "./style/layout/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<DummyLayout main={<HomePage />} />} />
-      <Route path="/board" element={<DummyLayout main={<BoardPage />} />} />
+      <Route path="/" element={<Layout main={<HomePage />} />} />
+      <Route path="/board" element={<Layout main={<BoardPage />} />} />
     </Routes>
   );
 }
