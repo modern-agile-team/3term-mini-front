94b1e710ea20f880e0e8fbdb268c84043db0e1a9
Lami <Halam.Lee.Lami@gmail.com>
2022-01-24 14:15:47 +0900

    Add(halam/Layout-footer):create-layout-footer #23

--- moae-time/src/style/layout/Layout.jsx
+++ moae-time/src/style/layout/Layout.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const WholeWrapper = styled.div`
   box-sizing: border-box;
@@ -7,7 +7,8 @@ const WholeWrapper = styled.div`
   padding: 0;
   width: 1512px;
   height: 982px;
-  background-color: #94f5dd;
+  background-color: #28b0b8;
+  position: relative;
 `;
 
 const HeaderBox = styled.div`
@@ -17,13 +18,25 @@ const HeaderBox = styled.div`
   width: 1512px;
   height: 80px;
   border-bottom: 0.5px solid #c4c4c4;
-  background: #FFFFFF;
-
+  background: #ffffff;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
+const FooterBox = styled.div`
+  box-sizing: border-box;
+  width: 1512px;
+  height: 50px;
+  background-color: #f9f9f9;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0;
+  padding: 0 400px;
+`;
 
 function Layout(props) {
   const { main } = props;
@@ -34,9 +47,13 @@ function Layout(props) {
         <div>2</div>
       </HeaderBox>
       {main}
+      <FooterBox>
+        <div>이용약관</div>
+        <div>개인정보처리방침</div>
+        <div>문의하기</div>
+        <div>모-애타임</div>
+      </FooterBox>
     </WholeWrapper>
   );
 }
-
-
 export default Layout;
