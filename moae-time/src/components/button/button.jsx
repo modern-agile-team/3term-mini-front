24277fb66c7bfb16ab0f622dfffdbf87fb240111
Lami <Halam.Lee.Lami@gmail.com>
2022-01-25 10:54:46 +0900

    Bug(halam/Layout-homepage):grid-bug #28

--- moae-time/src/components/button/button.jsx
+++ moae-time/src/components/button/button.jsx
@@ -1,23 +1,18 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const ButtonWrap = styled.button.attrs(({ height, width }) => ({
   row: height,
   col: width,
 }))`
-height: ${ (props) => props.row }
-width: ${ (props) => props.col };
-`
+  height: ${(props) => props.row};
+  width: ${(props) => props.col};
+`;
 
 function Button(props) {
-  const { height, width } = props
+  const { height, width } = props;
 
-  return (
-    <ButtonWrap 
-      height={height} 
-      width={width}
-    />
-  )
+  return <ButtonWrap height={height} width={width} />;
 }
 
 export default Button;
