5febfbb19ccd4348e7fdadf5be8ceba4b4e0c1e2
김성제 <rlatjdwp0211@naver.com>
2022-01-28 13:28:13 +0900

    Modify(sungje/Modal):creat-Modal #36

--- moae-time/src/components/smallmodal/SmallModal.jsx
+++ moae-time/src/components/smallmodal/SmallModal.jsx
@@ -1,6 +1,6 @@
-import { React, useEffect, useState, useRef } from 'react';
-import styled from 'styled-components';
-import { Box } from '../';
+import { React, useRef, useEffect, useState } from "react";
+import { Box } from "..";
+import styled from "styled-components";
 
 const Modal = styled.div.attrs(({ modalState, width, height, topMargin, leftMargin }) => ({
   state: modalState,
@@ -40,26 +40,6 @@ const ModalBackGround = styled.div.attrs(({ modalState }) => ({
   display: ${(props) => props.modalState ? "block" : "none"};
 
   background-color: rgba(97, 94, 94, 0.7);
-`
-
-const Wrap = styled.div`
-  width: 773.75px;
-  height: 432px;
-
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const CenterWrap = styled.div`
-  width: 269.91px;
-  height: 390px;
-
-  margin-left: 325.44px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 
 const ReportBoxAndTextWrap = styled.div`
@@ -72,7 +52,7 @@ const ReportBoxAndTextWrap = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-`
+`;
 
 const Btn = styled.button.attrs(({ height, width, margin }) => ({
   row: height,
@@ -84,33 +64,14 @@ const Btn = styled.button.attrs(({ height, width, margin }) => ({
   margin: ${(props) => props.margin};
 `;
 
-const BtnWrap = styled.div`
-  width: 100%;
-  height: fit-content;
 
-  margin-top: 61.25px;
 
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ReportBtnWrap = styled.div`
-  width: 19.5px;
-  height: 37.75px;
-
-  margin: 0px 0px 0px 69px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-function BigModal() {
+function SmallModal() {
   const modalEl = useRef();
-  const [modalState, setModalState] = useState(false);
   const [reportModalState, setReportModalState] = useState(false);
+
   const handleClickOutside = ({ target }) => {
-    if (modalState && !modalEl.current.contains(target)) setModalState(false);
+    if (reportModalState && !modalEl.current.contains(target)) setReportModalState(false);
   };
 
   useEffect(() => {
@@ -118,12 +79,7 @@ function BigModal() {
     return () => {
       window.removeEventListener("click", handleClickOutside);
     };
-  }, [modalState]);
-
-  useEffect(() => {
-    if (modalState) document.body.style.overflow = 'hidden';
-    else document.body.style.overflow = 'visible';
-  }, [modalState]);
+  }, [reportModalState]);
 
   const checkBoxNum = [1, 2, 3, 4, 5, 6, 7]
   const drawing = () => {
@@ -133,22 +89,14 @@ function BigModal() {
           <Box width="224px" height="16px" margin="0px 0px 20px 0px" />
       </ReportBoxAndTextWrap>
       )}
+
   return (
-    <>  
-      <Btn onClick={() => setModalState(true)}>O</Btn>
+    <>
       {
-        modalState &&
-      <Wrap>
-        <ModalBackGround modalState={modalState}>
-          <Modal
-            ref={modalEl} 
-            modalState={modalState}
-            width="907px"
-            height="590px"
-            topMargin="66px" 
-            leftMargin="303px">
+        reportModalState &&
           <ModalBackGround modalState={reportModalState}>
             <Modal 
+              ref={modalEl} 
               modalState={reportModalState}
               width="400px"
               height="480px"
@@ -166,33 +114,9 @@ function BigModal() {
               <Box width="46px" height="30px" margin="0px 0px 27px 177px"/>
             </Modal>
           </ModalBackGround>
-          <BtnWrap>
-            <ReportBtnWrap>
-              <Btn 
-                width="19.5px"
-                height="20.75px"
-                onClick={() => setReportModalState(true)}>R</Btn>
-              <Box width="19.5px" height="10px"/>
-            </ReportBtnWrap>
-            <Btn
-              width="15.5px"
-              height="15.5px" 
-              margin="0px 64.25px 0px 0px"
-              onClick={() => setModalState(false)}>X</Btn>
-          </BtnWrap>
-          <CenterWrap>
-            <Box width='200px' height='200px' margin="0px 0px 20px 0px"/>
-            <Box width='200px' height='32px' margin="0px 0px 51px 0px"/>
-            <Box width='232px' height='29px' margin="0px 0px 21px 0px"/>
-            <Box width='232px' height='29px' margin="0px 0px 21px 0px"/>
-            <Box width='232px' height='29px' margin="0px 0px 98px 0px"/>
-          </CenterWrap>
-          </Modal>
-        </ModalBackGround>
-      </Wrap>
       }
     </>
-  );
+  )
 }
 
-export default BigModal;
+export default SmallModal;
