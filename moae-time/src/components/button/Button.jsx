9c53877ea6da50e0a4a3d8a0c24aa5779244dac8
leephoter <estarg51@gmail.com>
2022-01-31 00:22:51 +0900

    Modify(hangyeol/Modal):added modal-router #36

---
+++ moae-time/src/components/button/Button.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonWrap = styled.button.attrs(
+  ({
+    width,
+    height,
+    bgColor = '#C62935',
+    fontSize = '18px',
+    fontColor = 'white',
+    borderRadius = '14px',
+  }) => ({
+    col: width,
+    row: height,
+    bgColor,
+    fontSize,
+    fontColor,
+    borderRadius,
+  })
+)`
+  width: ${(props) => props.col};
+  height: ${(props) => props.row};
+  background-color: ${(props) => props.bgColor};
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => props.fontColor};
+  border-radius: ${(props) => props.borderRadius};
+  border: none;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const Button = (props) => {
+  const {
+    width,
+    height,
+    bgColor,
+    fontSize,
+    fontColor,
+    borderRadius,
+    children,
+    onClick,
+  } = props;
+
+  return (
+    <ButtonWrap
+      height={height}
+      width={width}
+      bgColor={bgColor}
+      fontSize={fontSize}
+      fontColor={fontColor}
+      borderRadius={borderRadius}
+      onClick={onClick}
+    >
+      {children}
+    </ButtonWrap>
+  );
+};
+
+export default Button;
