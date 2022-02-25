cf8e04a2ce6854cff289e7f636e1eff0dc2f3d01
leephoter <estarg51@gmail.com>
2022-02-25 14:38:03 +0900

    modify(hangyeol/Layout-boardPage):modified create button function #30

--- moae-time/src/pages/board/BoardPageContainer.jsx
+++ moae-time/src/pages/board/BoardPageContainer.jsx
@@ -2,12 +2,8 @@ import { React, useState, useEffect } from 'react';
 import BoardPagePresenter from './BoardPagePresenter';
 import dummyBoard from '../../apis/dummyBoard.json';
 
-
-function BoardPage() {
-  
-  return (
-  <BoardPagePresenter />
-  );
+function BoardPage(props) {
+  return <BoardPagePresenter data={props} />;
 }
 
 export default BoardPage;
